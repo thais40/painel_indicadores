8b9a3f78d36ce61fda8cf3223da5767917be0089
thais40 <thais.santos@nuvemshop.com.br>
2025-08-18 18:25:44 -0300

    Update painel_jira_por_projeto_completo_CORRIGIDO_FINAL.py

--- painel_jira_por_projeto_completo_CORRIGIDO_FINAL.py
+++ painel_jira_por_projeto_completo_CORRIGIDO_FINAL.py
@@ -247,11 +247,10 @@ for projeto, tab in zip(PROJETOS, tabs):
                             y="Qtd",
                             color="origem_nome",
                             barmode="group",
-                            orientation="h",
                             title="APP NE — Volumes por mês e Origem do problema",
                             color_discrete_map={"APP NE": "#2ca02c", "APP EN": "#1f77b4"},
                             text="Qtd",
-                            height=700   # 👉 aumenta bastante a altura do gráfico
+                            height=900   # 👉 aumenta bastante a altura do gráfico
                         )
                         fig_app.update_traces(texttemplate="%{text}", textposition="outside")
                         fig_app.update_layout(yaxis_title="Qtd", xaxis_title="Mês")
